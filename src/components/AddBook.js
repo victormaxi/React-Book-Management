ec9a2d34aa35fb0b846199406d3b185462f8e6bc
kelechi onu <maximillianonu@gmail.com>
2021-10-31 23:12:24 +0100

    first commit

---
+++ src/components/AddBook.js
@@ -0,0 +1,23 @@
+import React, {useContext } from 'react';
+import BookForm from './BookForm';
+import BooksContext from '../context/BooksContext';
+
+const AddBook = ({history}) => {
+
+    const {books, setBooks} = useContext(BooksContext);
+
+    const handleOnSubmit = (book) => {
+        setBooks([book, ...books]);
+        history.push('/');
+        console.log(book);
+    };
+
+    return (
+        <React.Fragment>
+            <BookForm handleOnSubmit={handleOnSubmit} />
+            </React.Fragment>
+    );
+    
+};
+
+export default AddBook;
